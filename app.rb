5b9ccacf3f6046d33f9468afcca88de23c7af754
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-12-17 17:02:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,26 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
 
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "#{@name}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number]
+    squared = @number * @number
+  end
+
+  get '/say/:number/:phrase' do
+
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+
+  end
+
+  get '/:operation/:number1/:number2' do
+
+  end
 end
