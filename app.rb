39dd791c8f2400cbda6c8ace2136adb71279320f
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-12-17 17:26:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,19 +4,20 @@ class App < Sinatra::Base
 
   get '/reversename/:name' do
     @name = params[:name].reverse
-    "#{@name}"
   end
 
   get '/square/:number' do
-    @number = params[:number].to_i
-    squared = @number * @number
-    "#{squared}"
+    @squared = params[:number].to_i ** 2
+    "#{@squared}"
   end
 
   get '/say/:number/:phrase' do
-    @number = params[:number].to_i *
+    @number = params[:number].to_i
     @phrase = params[:phrase]
-    
+      @number.times do
+        @phrase
+      end
+    end
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
