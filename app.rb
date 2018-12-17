4b81bd7a4a76cb33d37e0e277cfe047be56b7ce5
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-12-17 17:17:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,10 +14,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @number = params[:number].to_i
-    @phrase = params[:phrase]
-    string = @number * @phrase
-    "#{string}"
+    
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
